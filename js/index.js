b14e2216021b2f9d8138f022343fcc5b2ebe60eb
gidkev <m.m.bhrm85@gmail.com>
2023-07-15 17:49:06 +0330

    Made the time Slider go by it's own

--- js/index.js
+++ js/index.js
@@ -9,9 +9,10 @@ class Player {
     this.volumeSlider = player.querySelector('#volume-slider')
     this.timeSlider = player.querySelector('#time-slider')
 
+    this.audio = document.createElement('audio')
     this.isPlaying = false
     this.musicIndex = 0
-    this.audio = document.createElement('audio')
+    this.currentTimeTracker;
     this.musics = [
       {
         "name": "مست نجف",
@@ -41,8 +42,6 @@ class Player {
     this.svgs = {
       pause: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M216 48v160a16 16 0 0 1-16 16h-40a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h40a16 16 0 0 1 16 16ZM96 32H56a16 16 0 0 0-16 16v160a16 16 0 0 0 16 16h40a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16Z"/></svg>',
       play: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128Z"/></svg>',
-      next: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M208 40v176a8 8 0 0 1-16 0v-69.23L72.43 221.55A15.95 15.95 0 0 1 48 208.12V47.88a15.95 15.95 0 0 1 24.43-13.43L192 109.23V40a8 8 0 0 1 16 0Z"/></svg>',
-      pervious: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M208 47.88v160.24a16 16 0 0 1-24.43 13.43L64 146.77V216a8 8 0 0 1-16 0V40a8 8 0 0 1 16 0v69.23l119.57-74.78A15.95 15.95 0 0 1 208 47.88Z"/></svg>',
     }
   }
   init = () => {
@@ -77,35 +76,54 @@ class Player {
     this.reset()
     this.musicIndex = (this.musicIndex <= 0) ? (this.musics.length - 1) : (this.musicIndex - 1);
     this.loadMusic(this.musicIndex)
+    this.plause()
   }
   reset = () => {
     this.btnPlay.innerHTML = this.svgs.play
     if (!this.audio.paused) this.audio.pause()
     this.isPlaying = false
     this.timeSlider.value = 0
+    this.setSliderStyle(0, this.timeSlider)
   }
   setEL = () => {
-    this.fixUI()
     this.audio.addEventListener('ended', this.next)
     this.btnPlay.addEventListener('click', this.plause)
     this.btnNext.addEventListener('click', this.next)
     this.btnPervious.addEventListener('click', this.pervious)
     this.volumeSlider.addEventListener('input', this.setVolume)
     this.timeSlider.addEventListener('input', this.setTime)
+    this.currentTimeTracker = setInterval(() => {
+      if (!this.audio.paused) this.setTimeUI(this.calcCurrentPercentage())
+    }, 1000)
   }
-  fixUI = () => {
-    this.btnPlay.innerHTML = this.svgs.play
-    this.btnNext.innerHTML = this.svgs.next
-    this.btnPervious.innerHTML = this.svgs.pervious
+  setSliderStyle = (value, elem) => {
+    elem.style.background = `linear-gradient(to right, hsl(200, 100%, 50%) ${value}%, hsla(0, 0%, 0%, 0.5) 0%)`
   }
+
   setVolume = () => {
-    this.audio.volume = this.volumeSlider.value / 100
-    this.volumeSlider.style.background = `linear-gradient(to right, hsl(200, 100%, 50%) ${this.volumeSlider.value}%, hsla(0, 0%, 0%, 0.5) 0%)`
+    this.setCurrentVolume(this.volumeSlider.value)
+    this.setVolumeUI(this.volumeSlider.value)
+  }
+  setCurrentVolume = (percentage) => {
+    this.audio.volume = percentage / 100
+  }
+  setVolumeUI = (percentage) => {
+    this.volumeSlider.setAttribute('value', percentage)
+    this.setSliderStyle(percentage, this.volumeSlider)
   }
   setTime = () => {
-    let timeToSeekTo = (this.audio.duration * (this.timeSlider.value / 100))
-    this.audio.currentTime = timeToSeekTo
+    this.setCurrentTime(this.timeSlider.value)
+    this.setTimeUI(this.timeSlider.value)
   }
+  setCurrentTime = (percentage) => {
+    this.audio.currentTime = (this.audio.duration * (percentage / 100))
+  }
+  setTimeUI = (percentage) => {
+    this.timeSlider.setAttribute('value', percentage)
+    this.timeSlider.value = percentage
+    this.setSliderStyle(percentage, this.timeSlider)
+  }
+  calcCurrentPercentage = () => Math.round((this.audio.currentTime / this.audio.duration) * 100)
 }
 
 const player = new Player(document.querySelector('.player'))
